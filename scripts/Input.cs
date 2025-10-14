46d4c2049cd7d94cc1ff8997f0c5a3e73cc2f58f
Hunt3rtk <htempleklassen@gmail.com>
2025-10-13 22:27:39 -0700

    Adding raycast ~ Made random selection of enemies and number of them.

---
+++ scripts/Input.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class Input : RayCast3D
+{
+	private const float RayLength = 1000.0f;
+
+	private Vector3 from;
+	private Vector3 to;
+
+	[Export]
+	Camera3D camera3D;
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed && eventMouseButton.ButtonIndex == MouseButton.Left)
+		{
+			GD.Print(" Input event: ", @event);
+			//camera3D = GetNode<Camera3D>("Camera3D");
+			from = camera3D.ProjectRayOrigin(eventMouseButton.Position);
+			to = from + camera3D.ProjectRayNormal(eventMouseButton.Position) * RayLength;
+			
+			var spaceState = GetWorld3D().DirectSpaceState;
+			var query = PhysicsRayQueryParameters3D.Create(from, to);
+			query.CollideWithAreas = true;
+
+			var result = spaceState.IntersectRay(query);
+			GD.Print("Hit at point: ", result);
+		}
+	}
+}
--- scripts/Spawn.cs
+++ scripts/Spawn.cs
@@ -4,17 +4,79 @@ using System;
 public partial class Spawn : Node
 {
 	[Export]
-	public PackedScene[] spawnScene;
+	PackedScene[] spawnScene;
+
+	[Export]
+	int minSpawn = 1;
+
+	[Export]
+	int maxSpawn = 5;
+
+	double timeout = 0;
+
+	double timeoutInterval = 2;
+
+	[Export]
+	public int round = 0;
+
+	double difficulty = 0;
+
+	double difficultyInterval = 10;
+	
+	[Export]
+	public int difficultyLevel = 0;
 
 	public override void _Process(double delta)
 	{
-		foreach (PackedScene scene in spawnScene)
+		timeout += delta;
+		difficulty += delta;
+
+		SetDifficulty();
+		SetRound();
+	}
+
+	void SetDifficulty()
+	{
+		if (difficulty > difficultyInterval)
 		{
-			Sprite3D sprite = scene.Instantiate<Sprite3D>();
-			sprite.Position = new Vector3((GD.Randf()-0.5f) * 10, (GD.Randf()-0.5f) * 10, 0);
-			
-			AddChild(sprite);
+			difficulty = 0;
+			if (timeoutInterval > 0.5)
+			{
+				difficultyLevel++;
+				timeoutInterval -= 0.1;
+			}
+
+		}
+	}
+	
+	void SetRound()
+	{
+		if (timeout > timeoutInterval)
+		{
+			round++;
+			timeout = 0;
+			SpawnScenes();
 		}
 	}
 
+	void SpawnScenes()
+	{
+		int spawnCount = GD.RandRange(minSpawn, maxSpawn);
+		for (int i = 0; i < spawnCount; i++)
+		{
+			int sceneIndex = (int)(GD.Randi() % spawnScene.Length);
+			PackedScene scene = spawnScene[sceneIndex];
+			if (scene == null)
+				continue;
+			SpawnScene(scene);
+		}
+	}
+	
+	void SpawnScene(PackedScene scene)
+		{
+			Sprite3D sprite = scene.Instantiate<Sprite3D>();
+			sprite.Position = new Vector3((GD.Randf() - 0.5f) * 20, (GD.Randf() - 0.5f) * 20, -20);
+
+			AddChild(sprite);
+		}
 }
